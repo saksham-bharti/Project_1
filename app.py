08139ca477907b8d4aa1e8e7ee8e90f1719401aa
Saksham Bharti <143437435+saksham-bharti@users.noreply.github.com>
2024-03-11 16:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def connect_db():
 oauth = OAuth(app)
 
 # github
-app.config['SECRET_KEY'] = "THIS SHOULD BE SECRET"
+app.config['SECRET_KEY'] = "Its the biggers tressure, hide it from others"
 app.config['GITHUB_CLIENT_ID'] = "3caee7f123efc248d725"
 app.config['GITHUB_CLIENT_SECRET'] = "cd017e28edefc4c26779425eb95551e5972a1caa"
 
